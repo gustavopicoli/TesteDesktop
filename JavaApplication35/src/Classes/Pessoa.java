1609b8b3bf67e7f3cea17a53c010934f1fb15285
“Gustavo <gustavo.picoli@hotmail.com>
2025-07-16 20:47:48 -0300

    Segundo commit

--- JavaApplication35/src/Classes/Pessoa.java
+++ JavaApplication35/src/Classes/Pessoa.java
@@ -10,4 +10,15 @@ package Classes;
  */
 public class Pessoa {
     
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    
 }
--- JavaApplication35/src/javaapplication35/JavaApplication35.java
+++ JavaApplication35/src/javaapplication35/JavaApplication35.java
@@ -1,8 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package javaapplication35;
+
+import Classes.Pessoa;
 
 /**
  *
@@ -10,11 +9,9 @@ package javaapplication35;
  */
 public class JavaApplication35 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+      
+        Pessoa pessoa = new Pessoa();
     }
     
 }
